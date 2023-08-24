c71607a2a415cbc3e6894fb09e1d52768f0cee0f
Adrian Restrepo <adriale0902@gmail.com>
2023-08-24 12:08:43 -0400

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './styles.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Board extends React.Component{
+  constructor(props){
+    super(props);
+    // state es el objeto con las variabes por defecto y se cambian con setState
+    this.state = {
+      squares: Array(9).fill(null),
+      isNextX: true,
+      isFinishSquares: false
+    }
+  }// variables de estado
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  renderSquares(i){
+    return(
+      <Square
+        props={this.state.squares[i]}
+      />
+    );
+  }
+
+  render(){
+    return(
+      <div className="container-board">
+        <div className="board-row">
+          {this.renderSquares(0)}
+          {this.renderSquares(1)}
+          {this.renderSquares(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquares(3)}
+          {this.renderSquares(4)}
+          {this.renderSquares(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquares(6)}
+          {this.renderSquares(7)}
+          {this.renderSquares(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+function Square(props){
+  return(
+    <button className="square" value={props.value}>
+      {props.value}
+    </button>
+  )
+}
+
+
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+Root.render(<Board />);
